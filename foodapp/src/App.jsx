9cd1d2097ecdcc62c45c50094503a5958451eb8c
jontot27 <j.a.ballesterosjr@gmail.com>
2024-08-03 01:19:55 +0800

    Building The Food List Component

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -7,9 +8,7 @@ function App() {
   return (
     <div className="App">
       <Search foodData={foodData} setFoodData={setFoodData} />
-      {foodData.map((food) => (
-        <h1>{food.title}</h1>
-      ))}
+      <FoodList foodData={foodData} />
     </div>
   );
 }
